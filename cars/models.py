2c8e53c57ce33798a5de5813d1db6ce148118d82
Maicon_Cunha <maicoboeira@hotmail.com>
2026-01-28 16:56:35 -0300

    adicionando app cars

---
+++ cars/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    factory_year = models.IntegerField()
+    model_year = models.IntegerField()
+    value = models.FloatField(blank=True, null=True)
+    
