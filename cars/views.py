fbc3cf335aec81d237dc523c861066e3170390c0
Maicon_Cunha <maicoboeira@hotmail.com>
2026-02-08 16:49:36 -0300

    adicionando html

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,11 @@
+print("CARREGOU cars.views CORRETO")
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def cars_view(request):
+    return render(
+        request,
+         'cars.html',
+         {'cars': {'model': 'Gol 1000 93'}}    
+    )
